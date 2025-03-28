05e45ff717a7de5ed80f18c6e6f5eed0643a67b9
eblopes23 <eblopes23@gmail.com>
2025-03-27 23:01:56 -0300

    Alterações do menu com a adição de testes da classe conta na classe menu

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -1,12 +1,19 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.2f);
+		c1.visualizar();
+		c1.sacar(12000.2f);
+		c1.visualizar();
+		c1.depositar(5000.2f);
+		c1.visualizar();
 		
 		Scanner sc = new Scanner(System.in);
 		
